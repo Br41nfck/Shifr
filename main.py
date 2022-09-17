3b9f9356beb676beeeab51797666e4ea799f1e7e
Trix <georgyinjapan3@gmail.com>
2022-09-17 12:58:29 +0300

    Shifr - Version: 0.0.3 Date: 17/09/2022

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# Version 0.0.2
+# Version 0.0.3
 # Last update: 17.09.2022
 # UPDATES:
 # 1. Add "Reverse string" Method
-# 2.
-# n.
+# 2. Add "SHA-1" Algorithm
+# 3. Add except handler
 from tkinter import *
 from Algs.reverse_string import reverse
+from Algs.sha1 import sha1_crypt
 import base64
+
 
 root = Tk()
 
@@ -24,11 +26,12 @@ lblInfo = Label(Tops, text = "Message Encrypt/Decrypt \n", anchor = 'w')
 lblInfo.grid(row = 1, column = 0)
 
 rand = StringVar()
-rvrt = StringVar()
+rvrs = StringVar()
 Msg = StringVar()
 key = StringVar()
 mode = StringVar()
 Result = StringVar()
+sha_1 = StringVar()
 
 
 # exit function
@@ -43,21 +46,22 @@ def reset():
     key.set("")
     mode.set("")
     Result.set("")
-    rvrt.set("")
+    rvrs.set("")
+    sha_1.set("")
 
 
-# labels
+# Labels
 lbl_Msg = Label(f1, text = "Message", anchor = "w")
 lbl_Msg.grid(row = 1, column = 0)
 
 txt_Msg = Entry(f1, textvariable = Msg, insertwidth = 4)
 txt_Msg.grid(row = 1, column = 1)
 
-lbl_rvrt = Label(f1, text = "Revert", anchor = "w")
-lbl_rvrt.grid(row = 1, column = 3)
+lbl_rvrs = Label(f1, text = "Revert", anchor = "w")
+lbl_rvrs.grid(row = 1, column = 3)
 
-txt_rvrt = Entry(f1, textvariable = rvrt, insertwidth = 4)
-txt_rvrt.grid(row = 1, column = 4)
+txt_rvrs = Entry(f1, textvariable = rvrs, insertwidth = 4)
+txt_rvrs.grid(row = 1, column = 4)
 
 lbl_key = Label(f1, text = "Key", bd = 16, anchor = "w")
 lbl_key.grid(row = 2, column = 0)
@@ -71,11 +75,17 @@ lbl_mode.grid(row = 3, column = 0)
 txt_mode = Entry(f1, textvariable = mode, insertwidth = 4)
 txt_mode.grid(row = 3, column = 1)
 
-lblService = Label(f1, text = "Result", anchor = "w")
-lblService.grid(row = 2, column = 3)
+lbl_sha_1 = Label(f1, text = "SHA-1", anchor = "w")
+lbl_sha_1.grid(row = 2, column = 3)
 
-txtService = Entry(f1, textvariable = Result, insertwidth = 4)
-txtService.grid(row = 2, column = 4)
+txt_sha_1 = Entry(f1, textvariable = sha_1, insertwidth = 4)
+txt_sha_1.grid(row = 2, column = 4)
+
+lbl_res = Label(f1, text = "Result", anchor = "w")
+lbl_res.grid(row = 3, column = 3)
+
+txt_res = Entry(f1, textvariable = Result, insertwidth = 4)
+txt_res.grid(row = 3, column = 4)
 
 
 # Function to encode
@@ -100,16 +110,23 @@ def decode(key_, enc):
 
 
 def callback():
-    msg = Msg.get()
-    print("Message = ", msg)
-    rvrt.set(reverse(msg))
-    clear = msg
-    k = key.get()
-    m = mode.get()
-    if m == 'e':
-        Result.set(encode(k, clear))
-    else:
-        Result.set(decode(k, clear))
+    try:
+        msg = Msg.get()
+        # print("Message = ", msg)
+        rvrs.set(reverse(msg))
+        sha_1.set(sha1_crypt(msg))
+        clear = msg
+        k = key.get()
+        m = mode.get()
+        if m == 'e':
+            Result.set(encode(k, clear))
+        else:
+            Result.set(decode(k, clear))
+
+    except Exception as e:
+        f = open('logs.log', 'a')
+        f.write(str(e)+'\n')
+        f.close()
 
 
 # Show message button
@@ -123,6 +140,7 @@ btnReset.grid(row = 4, column = 2)
 # Exit button
 btnExit = Button(f1, text = "Exit", command = qexit)
 btnExit.grid(row =4, column = 3)
+
 
 # keeps window alive
 root.mainloop()
